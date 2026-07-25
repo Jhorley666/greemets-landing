bdccc805d68af10ef7becf9cadb9f8ad4fa7f8d1
Rolando Jhorley Atuña Ordoñez <jhorley1998@gmail.com>
2026-07-25 15:47:21 -0500

    Actualizar astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // Configure for GitHub Pages. Change 'greemets-landing' if your repository is named differently.
-  site: 'https://greemets.github.io',
-  base: '/greemets-landing',
+  site: 'https://greemets.net',
 });
